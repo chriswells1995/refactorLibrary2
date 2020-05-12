a1a85c9fc5c8c26cd03f3e0609edfc19993d64ba
Chris W <wells457@umn.edu>
2020-05-12 10:04:03 -0500

    heroku stuff

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,22 @@
 const mysql = require('mysql');
 // We require util so we can promisify
 const util = require('util');
-
+var connection;
 // we create the connection to mysql using my username and password, accessing the library_db databse
-const connection = mysql.createConnection({
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} 
+else {
+connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: '',
     database: 'library_db'
   });
+
+}
+
 // we catch any errors
 connection.connect(function(err) {
   if (err) {
